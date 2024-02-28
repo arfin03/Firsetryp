518fa808f7ba2557dee77b013526f25978e108ae
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-02-28 17:27:44 +0530

    Update Top.py

--- shivu/modules/Top.py
+++ shivu/modules/Top.py
@@ -4,42 +4,32 @@ from urllib.parse import quote_plus
 from shivu import user_collection, application, image_urls
 import random
 
-async def fetch_user_profile(user_id):
-    # Fetch user profile from the database based on user_id
-    user_profile = await user_collection.find_one({'_id': user_id})
-    return user_profile
 
 async def top(update: Update, context: CallbackContext):
-    # Retrieve users along with their character counts
-    users = await user_collection.find().to_list(length=None)
+    
+    users = await user_collection.find().to_list(length=None)
 
-    # Sort users by character counts in descending order
-    top_users = sorted(users, key=lambda user: len(user.get('characters', [])), reverse=True)[:10]
+    top_users = sorted(users, key=lambda user: len(user.get('characters', [])), reverse=True)[:10]
 
-    if top_users:
-        # Generate the message
-        message = "<b>Top 10 Users by Number of Characters:</b>\n\n"
-        for idx, user in enumerate(top_users, start=1):
-            character_count = len(user.get('characters', []))
-            
-            # Fetch user profile details from the database based on ID (user["_id"])
-            # Replace this line with your actual code to fetch user profile details
-            user_profile = await fetch_user_profile(user["_id"])
-            
-            # If user profile is found, get the username
-            if user_profile:
-                username = user_profile.get('username', 'Unknown')
-                user_link = f'<a href="tg://user?id={user["_id"]}">{username}</a>'
-            else:
-                # If user profile is not found, use a placeholder
-                user_link = "Unknown User"
-            
-            message += f"{idx}. {user_link}: {character_count}\n"
+    if top_users:
+        
+        image_url = random.choice(image_urls)
 
-        # Send the message
-        await update.message.reply_text(message, parse_mode='HTML')
-    else:
-        await update.message.reply_text("No users found.")
+        message = "<b>Top 10 Users by Number of Characters:</b>\n\n"
+        for idx, user in enumerate(top_users, start=1):
+            character_count = len(user.get('characters', []))
+            username = user.get('username')
+            if username:
+                user_link = f'<a href="https://t.me/{username}">{username}</a>'
+            else:
+                first_name = user.get('first_name', 'Unknown')
+                user_link = f'<b>{first_name}</b>'
+            message += f"{idx}. {user_link}: {character_count}\n"
+
+        await update.message.reply_photo(photo=image_url, caption=message, parse_mode='HTML')
+    else:
+        await update.message.reply_text("No users found.")
+
 
 # Define the command handler for /top command
 top_handler = CommandHandler('top', top)

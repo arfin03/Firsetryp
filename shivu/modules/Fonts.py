e0136c758e4feafe9a04cf862b1c52db7171654e
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-02-28 21:23:04 +0530

    Delete shivu/modules/Fonts.py

--- shivu/modules/Fonts.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Get the absolute path to the Arial font file
-FONT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-ARIAL_FONT_PATH = os.path.join(FONT_DIRECTORY, "arial.ttf")

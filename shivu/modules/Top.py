0c672faf56bd398254012870a8b1f252bd8c0224
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-02-28 22:27:27 +0530

    Update Top.py

--- shivu/modules/Top.py
+++ shivu/modules/Top.py
@@ -1,9 +1,8 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, CallbackContext
-from urllib.parse import quote_plus
 from shivu import user_collection, application, image_urls
 import random
-
+from urllib.parse import quote_plus
 
 async def top(update: Update, context: CallbackContext):
     users = await user_collection.find().to_list(length=None)
@@ -14,23 +13,24 @@ async def top(update: Update, context: CallbackContext):
         image_url = random.choice(image_urls)
 
         message = "<b>Top 10 Users by Number of Characters:</b>\n\n"
+        buttons = []
         for idx, user in enumerate(top_users, start=1):
             character_count = len(user.get('characters', []))
+            first_name = user.get('first_name', 'Unknown')
             username = user.get('username')
-            if username:
-                user_link = f'<a href="https://t.me/{username}">{username}</a>'
-            else:
-                first_name = user.get('first_name', 'Unknown')
-                user_link = f'<b>{first_name}</b>'
+            user_link = f'<a href="https://t.me/{username}">{first_name}</a>' if username else f'<b>{first_name}</b>'
             message += f"{idx}. {user_link}: {character_count}\n"
-
-        await update.message.reply_photo(photo=image_url, caption=message, parse_mode='HTML')
+            # Create a button for each user's first name
+            buttons.append([InlineKeyboardButton(first_name, url=f"https://t.me/{username}")])
+        
+        keyboard = InlineKeyboardMarkup(buttons)
+        await update.message.reply_photo(photo=image_url, caption=message, parse_mode='HTML', reply_markup=keyboard)
     else:
         await update.message.reply_text("No users found.")
-
 
 # Define the command handler for /top command
 top_handler = CommandHandler('top', top)
 
 # Add the command handler to the dispatcher
 application.add_handler(top_handler)
+    

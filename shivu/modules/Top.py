e11408b4954219d51d9bfccd2b04b2ca5f15ca4f
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-02-28 17:34:32 +0530

    Update Top.py

--- shivu/modules/Top.py
+++ shivu/modules/Top.py
@@ -6,28 +6,27 @@ import random
 
 
 async def top(update: Update, context: CallbackContext):
- users = await user_collection.find().to_list(length=None)
+    users = await user_collection.find().to_list(length=None)
 
-    top_users = sorted(users, key=lambda user: len(user.get('characters', [])), reverse=True)[:10]
+    top_users = sorted(users, key=lambda user: len(user.get('characters', [])), reverse=True)[:10]
 
-    if top_users:
-        
-        image_url = random.choice(image_urls)
+    if top_users:
+        image_url = random.choice(image_urls)
 
-        message = "<b>Top 10 Users by Number of Characters:</b>\n\n"
-        for idx, user in enumerate(top_users, start=1):
-            character_count = len(user.get('characters', []))
-            username = user.get('username')
-            if username:
-                user_link = f'<a href="https://t.me/{username}">{username}</a>'
-            else:
-                first_name = user.get('first_name', 'Unknown')
-                user_link = f'<b>{first_name}</b>'
-            message += f"{idx}. {user_link}: {character_count}\n"
+        message = "<b>Top 10 Users by Number of Characters:</b>\n\n"
+        for idx, user in enumerate(top_users, start=1):
+            character_count = len(user.get('characters', []))
+            username = user.get('username')
+            if username:
+                user_link = f'<a href="https://t.me/{username}">{username}</a>'
+            else:
+                first_name = user.get('first_name', 'Unknown')
+                user_link = f'<b>{first_name}</b>'
+            message += f"{idx}. {user_link}: {character_count}\n"
 
-        await update.message.reply_photo(photo=image_url, caption=message, parse_mode='HTML')
-    else:
-        await update.message.reply_text("No users found.")
+        await update.message.reply_photo(photo=image_url, caption=message, parse_mode='HTML')
+    else:
+        await update.message.reply_text("No users found.")
 
 
 # Define the command handler for /top command

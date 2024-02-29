30ab17e2ea7d590cb6b6781614e19e2949a5716b
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-03-01 01:11:01 +0530

    Update Haremmode.py

--- shivu/modules/Haremmode.py
+++ shivu/modules/Haremmode.py
@@ -90,8 +90,7 @@ async def rarity2_callback(client, callback_query):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
 
-        query.edit_message_caption(caption=message, reply_markup=reply_markup)
-
+     await query.edit_message_caption(caption=message, reply_markup=reply_markup)
 
 @app.on_callback_query(filters.regex('^(next|prev)_'))
 async def next_or_back_callback(client, callback_query):

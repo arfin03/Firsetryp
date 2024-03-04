e4955f5d79ef4b7b37be530e425f71057e130617
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-03-04 12:21:19 +0530

    Update Group0.py

--- shivu/modules/Group0.py
+++ shivu/modules/Group0.py
@@ -13,7 +13,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 # ID of the logging group
 logging_group_id = -1002059626060
 
-def welcome_new_member(update: Update, context):
+async def welcome_new_member(update: Update, context):
     # Extract relevant information from the update
     new_members = update.message.new_chat_members
     added_by = update.message.from_user
@@ -22,12 +22,11 @@ def welcome_new_member(update: Update, context):
 
     # Send a welcome message to the group
     welcome_message = f"Hello! I'm {context.bot.username}. Thank you for adding me to {chat_title}."
-    context.bot.send_message(chat_id, welcome_message)
+    await context.bot.send_message(chat_id, welcome_message)
 
     # Log information about the group and who added the bot in the logging group
     log_message = f"New group: {chat_title} (ID: {chat_id}). Added by: {added_by.first_name} (ID: {added_by.id})."
-    context.bot.send_message(logging_group_id, log_message)
-
+    await context.bot.send_message(logging_group_id, log_message)
 
 
     # Register handler for new chat members

a44e3b673acfc7b024907b6d5de2071ef3ec4be5
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-03-04 12:17:04 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -272,20 +272,7 @@ async def fav(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text(f'Character {character["name"]} has been added to your favorite...')
     
 
-def welcome_new_member(update: Update, context):
-    # Extract relevant information from the update
-    new_members = update.message.new_chat_members
-    added_by = update.message.from_user
-    chat_id = update.message.chat.id
-    chat_title = update.message.chat.title
 
-    # Send a welcome message to the group
-    welcome_message = f"Hello! I'm {context.bot.username}. Thank you for adding me to {chat_title}."
-    context.bot.send_message(chat_id, welcome_message)
-
-    # Log information about the group and who added the bot in the logging group
-    log_message = f"New group: {chat_title} (ID: {chat_id}). Added by: {added_by.first_name} (ID: {added_by.id})."
-    context.bot.send_message(logging_group_id, log_message)
 
 
 
@@ -295,7 +282,7 @@ def main() -> None:
     application.add_handler(CommandHandler(["guess", "protecc", "collect", "grab", "hunt"], guess, block=False))
     application.add_handler(CommandHandler("fav", fav, block=False))
     application.add_handler(MessageHandler(filters.ALL, message_counter, block=False))
-    application.add_handler(MessageHandler(Filters.status_update.new_chat_members, welcome_new_member))
+   
 
    
     application.run_polling(drop_pending_updates=True)

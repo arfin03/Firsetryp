c05e02bf94e3c2e9a6d5e4e610098e9623dc146d
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-03-04 15:12:36 +0530

    Update Y.py

--- shivu/modules/Y.py
+++ shivu/modules/Y.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, MessageHandler, filters
+from telegram.ext import MessageHandler, filters
 import logging
 
 from shivu import application 
@@ -8,11 +8,11 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 # Define message handler
-def handle_promotion(update, context):
+async def handle_promotion(update, context):
     promoting_user = update.message.from_user
     if update.message.reply_to_message:
         promoted_user = update.message.reply_to_message.from_user
-        update.message.reply_text(f"{promoting_user.username} promoted {promoted_user.username}.")
+        await update.message.reply_text(f"{promoting_user.username} promoted {promoted_user.username}.")
 
 # Add message handler
-application.add_handler(MessageHandler(filters.reply & filters.text & ~filters.user(username="dark_waifu_bot"), handle_promotion))
+await application.add_handler(MessageHandler(filters.Filters.reply & filters.Filters.text & ~filters.Filters.user(username="dark_waifu_bot"), handle_promotion))

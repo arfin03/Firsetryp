3805a9d0ea33a26a67c312e0fe60a45e49f3f270
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-03-08 18:41:48 +0530

    Update harem.py

--- shivu/modules/harem.py
+++ shivu/modules/harem.py
@@ -85,29 +85,13 @@ async def harem(update: Update, context: CallbackContext, page=0) -> None:
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-if 'favorites' in user and user['favorites']:
-    fav_character_id = user['favorites'][0]
-    fav_character = next((c for c in user['characters'] if c['id'] == fav_character_id), None)
+    if 'favorites' in user and user['favorites']:
+        fav_character_id = user['favorites'][0]
+        fav_character = next((c for c in user['characters'] if c['id'] == fav_character_id), None)
 
-    if fav_character and 'img_url' in fav_character and fav_character['img_url']:
-        if update.message:
-            await update.message.reply_photo(photo=fav_character['img_url'], parse_mode='HTML', caption=harem_message, reply_markup=reply_markup)
-        else:
-            if update.callback_query.message.caption != harem_message:
-                await update.callback_query.edit_message_caption(caption=harem_message, reply_markup=reply_markup, parse_mode='HTML')
-    else:
-        if update.message:
-            await update.message.reply_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
-        else:
-            if update.callback_query.message.text != harem_message:
-                await update.callback_query.edit_message_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
-else:
-    if user['characters']:
-        random_character = random.choice(user['characters'])
-
-        if 'img_url' in random_character and random_character['img_url']:
+        if fav_character and 'img_url' in fav_character and fav_character['img_url']:
             if update.message:
-                await update.message.reply_photo(photo=random_character['img_url'], parse_mode='HTML', caption=harem_message, reply_markup=reply_markup)
+                await update.message.reply_photo(photo=fav_character['img_url'], parse_mode='HTML', caption=harem_message, reply_markup=reply_markup)
             else:
                 if update.callback_query.message.caption != harem_message:
                     await update.callback_query.edit_message_caption(caption=harem_message, reply_markup=reply_markup, parse_mode='HTML')
@@ -118,10 +102,24 @@ else:
                 if update.callback_query.message.text != harem_message:
                     await update.callback_query.edit_message_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
     else:
-        if update.message:
-            await update.message.reply_text("Your List is Empty :)")
+        if user['characters']:
+            random_character = random.choice(user['characters'])
 
-
+            if 'img_url' in random_character and random_character['img_url']:
+                if update.message:
+                    await update.message.reply_photo(photo=random_character['img_url'], parse_mode='HTML', caption=harem_message, reply_markup=reply_markup)
+                else:
+                    if update.callback_query.message.caption != harem_message:
+                        await update.callback_query.edit_message_caption(caption=harem_message, reply_markup=reply_markup, parse_mode='HTML')
+            else:
+                if update.message:
+                    await update.message.reply_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
+                else:
+                    if update.callback_query.message.text != harem_message:
+                        await update.callback_query.edit_message_text(harem_message, parse_mode='HTML', reply_markup=reply_markup)
+        else:
+            if update.message:
+                await update.message.reply_text("Your List is Empty :)")
 
 async def harem_callback(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
@@ -141,3 +139,5 @@ async def harem_callback(update: Update, context: CallbackContext) -> None:
 application.add_handler(CommandHandler(["harem", "collection"], harem, block=False))
 harem_handler = CallbackQueryHandler(harem_callback, pattern='^harem', block=False)
 application.add_handler(harem_handler)
+
+

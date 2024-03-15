1ee9297d2ff4dc7e28433baaa7462156dcfabc66
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-03-15 08:55:29 +0530

    Create Tgm.py

---
+++ shivu/modules/Tgm.py
@@ -0,0 +1,38 @@
+from telegraph import upload_file
+from pyrogram import filters
+from shivu import shivuu
+from pyrogram.types import InputMediaPhoto
+
+app = shivuu
+
+@app.on_message(filters.command(["tgm" , "telegraph"]))
+def ul(_, message):
+    reply = message.reply_to_message
+    if reply.media:
+        i = message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://telegra.ph" + x
+
+        i.edit(f'Yᴏᴜʀ ʟɪɴᴋ sᴜᴄᴄᴇssғᴜʟ Gᴇɴ `{url}`')
+
+########____________________________________________________________######
+
+@app.on_message(filters.command(["graph" , "grf"]))
+def ul(_, message):
+    reply = message.reply_to_message
+    if reply.media:
+        i = message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://graph.org" + x
+
+        i.edit(f'Yᴏᴜʀ ʟɪɴᴋ sᴜᴄᴄᴇssғᴜʟ Gᴇɴ `{url}`')
+
+await message.reply_text(
+        text,
+        disable_web_page_preview=True,
+        parse_mode=ParseMode.DEFAULT,
+)

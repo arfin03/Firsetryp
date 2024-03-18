9ec46f6cf93124abb59bb02da7184be5c72fc14d
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-03-18 10:12:25 +0530

    Update Clone.py

--- shivu/modules/Clone.py
+++ shivu/modules/Clone.py
@@ -7,8 +7,6 @@ from pyrogram.types import *
 from random import choice
 from shivu import shivuu
 
-
-
 API_HASH = "ec69ce8b56c71541499c914fabd08286"
 API_ID = "25635673"
 ASSUSERNAME = "not allowed"
@@ -17,19 +15,21 @@ app = shivuu
 IMG = ["https://graph.org/file/1aaff3780b5ba59cb1f63.jpg", "https://graph.org/file/1aaff3780b5ba59cb1f63.jpg", "https://graph.org/file/1aaff3780b5ba59cb1f63.jpg", "https://graph.org/file/1aaff3780b5ba59cb1f63.jpg"]
 MESSAGE = "Heya! I'm a music bot hoster/Cloner\n\nI can Host Your Bot On My Server within seconds\n\nTry /clone Token from @botfather"
 
+# Add debugging statements to see if modules are being loaded
+print("Modules to load:", shivuu.modules.__init__)
+
 @app.on_message(filters.private & filters.command("copy"))
 async def copy(client, message: Message):
     buttons = [
-           [
-                InlineKeyboardButton("✘ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ", url="t.me/"),
-            ],
-            [
-                InlineKeyboardButton("✘ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ", url="t.me/"),
-            ],
-            ]
+        [
+            InlineKeyboardButton("✘ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ", url="t.me/"),
+        ],
+        [
+            InlineKeyboardButton("✘ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ", url="t.me/"),
+        ],
+    ]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_photo(message.chat.id, f"{choice(IMG)}", caption=MESSAGE, reply_markup=reply_markup)
-
 
 @app.on_message(filters.private & filters.command("clone"))
 async def clone(bot, msg: Message):
@@ -39,10 +39,15 @@ async def clone(bot, msg: Message):
     phone = msg.command[1]
     try:
         await text.edit("Booting Your Client")
-                   # change this Directry according to ur repo
+        # Change this directory according to your repo
         client = Client(name="Melody", api_id=API_ID, api_hash=API_HASH, bot_token=phone, plugins=dict(root="shivu/modules"))
+        print("Client created successfully.")
         await client.start()
         user = await client.get_me()
         await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @{ASSUSERNAME} To Your Chat!\n\nThanks for Cloning.")
     except Exception as e:
+        print("Error:", e)
         await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
+
+# Add a debug statement to indicate that the script has started
+print("Script started.")

f4201e39f7054a5d81e60885a916e7ea7441530b
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-04-08 12:17:37 +0530

    Update shop.py

--- shivu/modules/shop.py
+++ shivu/modules/shop.py
@@ -11,8 +11,7 @@ shop_message_data = {}
 user_collection = {}  # Placeholder for user_collection
 
 @app.on_message(filters.command("shop"))
-async def shop_command(client, message, update):
-    global shop_message
+async def shop_command(_, update, message):
     
     rarity_3_characters = await collection.find({'rarity': "💸 Premium Edition"}).to_list(length=7)
 

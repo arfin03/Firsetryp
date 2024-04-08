fb9289898884a6994359424cf2f7e6d1cb65dea0
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-04-08 09:52:36 +0530

    Update shop.py

--- shivu/modules/shop.py
+++ shivu/modules/shop.py
@@ -15,14 +15,7 @@ user_collection = {}  # Placeholder for user_collection
 async def shop_command(_, update):
     rarity_3_characters = await collection.find({'rarity': "💸 Premium Edition"}).to_list(length=7)
 
-    try:
-        # Store data associated with the message using Pyrogram's data attribute
-        shop_message_info = {
-            'chat_id': update.chat.id,
-            'message_id': message.message_id,  # Use message_id from the response
-            'current_index': 0,
-            'user_id': update.effective_user.id
-        }
+
 
     if not rarity_3_characters:
         await update.reply_text("No legendary characters available in the shop.")
@@ -41,6 +34,15 @@ async def shop_command(_, update):
                 f"💸Coin Price: {first_character['coin_price']}",
         reply_markup=reply_markup
     )
+
+  try:
+        # Store data associated with the message using Pyrogram's data attribute
+        shop_message_info = {
+            'chat_id': message.chat.id,
+            'message_id': message.message_id,  # Use message_id from the response
+            'current_index': 0,
+            'user_id': update.effective_user.id
+        }
         
         # Update user_data with the message information
         shop_message_data[message.message_id] = shop_message_info
